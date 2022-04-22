9b1fc00f06774b81b61480aa993cd3e171b3feaa
Jonathan Barsky <barskyjonathan96@gmail.com>
2022-04-21 22:47:59 -0300

    card hecha

---
+++ src/app.js
@@ -0,0 +1,3 @@
+import './components/productCard.js';
+
+// si hubiese una lista de componentes se deberian hacer las llamadas desde un solo archivo .js(desde aca por ejemplo) porque sino se acumularian los scripts en el html 
